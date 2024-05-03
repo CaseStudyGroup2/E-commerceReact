b7ae3a92fe65e018f96c5737270566e20ba7210d
katharine <asuncionmarykatharine910@gmail.com>
2024-05-03 19:13:41 +0800

    update again

--- src/components/CartSummary.js
+++ src/components/CartSummary.js
@@ -1,9 +1,19 @@
+import React from 'react';
 
 const CartSummary = ({ cart }) => {
   // Calculate total items in the cart
   const totalItems = cart.reduce((acc, curr) => acc + curr.quantity, 0);
 
-  
+  // Calculate total price in the cart
+  const totalPrice = cart.reduce((acc, curr) => acc + curr.price * curr.quantity, 0).toFixed(2);
+
+  return (
+    <div>
+      <h2>Cart Summary</h2>
+      <p>Total Items: {totalItems}</p>
+      <p>Total Price: Php {totalPrice}</p>
+    </div>
+  );
 };
 
 export default CartSummary;
--- src/components/ProductInfo.js
+++ src/components/ProductInfo.js
@@ -1,10 +1,16 @@
+import React from 'react';
 
 const ProductInfo = ({ product, addToCart }) => {
   const { name, price, description } = product;
 
-  return 
-    
-
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p><strong>Price:</strong> Php{price}</p>
+      <p>Description: {description}</p>
+      <button onClick={addToCart}>Add to Cart</button>
+    </div>
+  );
 };
 
 export default ProductInfo;
